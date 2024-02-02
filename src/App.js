3b71821b91512a9372b2485bc316486a50ef04d6
shreyash-shetty <shettyshreyash201@gmail.com>
2024-02-02 13:19:37 +0530

    remove browserrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import {Routes,Route} from "react-router-dom"
 
 
 
 function App() {
   return (
   <>
-<Router>
+
    <Navbar/>
    <Routes>
-    <Route path="/"  element={<Home/>}/>     
-   <Route path="/about"  element={<About/>}/>
+    <Route path="/*"  element={<Home/>}/>     
+   <Route path="/about*"  element={<About/>}/>
    </Routes>
-   </Router>
+   
    </>  
   );
 }
